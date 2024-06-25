df318386d6013ed779ee0a708c36f0d6cb3b3502
Chloé Debyser <chloe.debyser@gmail.com>
2024-06-25 14:34:56 -0400

    Only add space after "sp." in scientific name if there is not already one

--- KBA Functions.R
+++ KBA Functions.R
@@ -258,7 +258,7 @@ read_KBACanadaProposalForm <- function(formPath, final){
     arrange(`Scientific name`)
   
         # Handle scientific names of the type "sp. #"
-  species %<>% mutate(`Scientific name` = trimws(gsub(" sp.", " sp. ", `Scientific name`, fixed=T)))
+  species$`Scientific name` <- sapply(species$`Scientific name`, function(x) ifelse(!grepl(" sp. ", x, fixed=T), trimws(gsub(" sp.", " sp. ", `Scientific name`, fixed=T)), x))
   
         # Handle differing form versions
   if(formVersion < 1.2){
